2f6fca26818e9f79a78804760298f86837ea9d3b
Serg3 <jaguar.sergey@gmail.com>
2018-02-28 20:09:30 +0300

    remove routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-
-  get '/public/about', page: 'about'
-  get '/public/author', page: 'author'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
