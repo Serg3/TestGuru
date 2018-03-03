6ece09edffcb841536991a3e0544187ba326bd46
Serg3 <jaguar.sergey@gmail.com>
2018-03-03 13:34:42 +0300

    add methods to classes Test and User

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.by_category(title)
+    joins(:category).where(categories: { title: title }).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def passed_tests(level)
+    Test.select(:title).where('level = ?', level)
+  end
 end
