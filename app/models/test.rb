270a3d39b0f5f2665a712403d4da6308e8fcc3be
Serg3 <jaguar.sergey@gmail.com>
2018-03-06 22:15:21 +0300

    replace class-method to scope-method

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,11 +3,9 @@ class Test < ApplicationRecord
   has_many :users , through: :test_passages
   belongs_to :category
 
+  scope :by_level, ->(level) { where(level: level) }
+
   def self.by_category(title)
     joins(:category).where(categories: { title: title }).order(title: :desc).pluck(:title)
-  end
-
-  def self.by_level(level)
-    where(level: level)
   end
 end
