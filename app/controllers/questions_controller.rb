699bd2fe1bec67799f994427ff725fb86434848a
Serg3 <jaguar.sergey@gmail.com>
2018-03-15 21:23:17 +0300

    edit standard actions for Question, add footer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,19 +1,22 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: [:index, :new, :create]
-  before_action :find_question, only: :destroy
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
+    @questions = @test.questions
   end
 
   def show
-    @question = Question.find(params[:id])
     @answers = @question.answers
   end
 
   def new
     @question = @test.questions.build
+  end
+
+  def edit
   end
 
   def create
@@ -25,9 +28,18 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def update
+    @question = @test.questions.build(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   def destroy
     @question.destroy
-    render plain: 'Question deleted.'
+    redirect_to question_path
   end
 
   private
