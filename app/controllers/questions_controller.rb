28e9d4accb63c1a33253e4f71c363903dfa7abd1
Serg3 <jaguar.sergey@gmail.com>
2018-03-17 00:36:39 +0300

    remove action index from questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,13 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :new, :create]
+  # before_action :find_test, only: [:index, :new, :create]
+  before_action :find_test, only: [:new, :create]
   before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    @questions = @test.questions
-  end
+  # def index
+  #   @questions = @test.questions
+  # end
 
   def show
     @answers = @question.answers
