d763f9d2729d017eca6815fdc4e35e7ace457e93
Serg3 <jaguar.sergey@gmail.com>
2018-03-26 22:37:42 +0300

    edit permitted users parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(user)
     user.admin? ? admin_tests_path : tests_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
