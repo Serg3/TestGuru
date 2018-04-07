ae4ca05e6e36aad75af310b25e966ed90a1c6451
Serg3 <jaguar.sergey@gmail.com>
2018-04-07 21:43:15 +0300

    rename remote app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'sheltered-sierra-96976.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'test-guru-sk-96976.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     adress:               'smtp.gmail.com',
