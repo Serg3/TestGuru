452ebd95abd5c8dbb55ffb6c0a5a7708cd43a8b4
Serg3 <jaguar.sergey@gmail.com>
2018-04-09 21:29:57 +0300

    realize feedbacks via inner mailer

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
+  resources :feedbacks, only: %i[new create]
+
   resources :tests, only: :index do
     member do
       post :start
