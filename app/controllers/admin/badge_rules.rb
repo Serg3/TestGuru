3c3e6dc639cc1ba4dd783f64ca890b1e65f663d1
Serg3 <jaguar.sergey@gmail.com>
2018-04-11 23:06:01 +0300

    add rules for reward

---
+++ app/controllers/admin/badge_rules.rb
@@ -0,0 +1,48 @@
+class BadgeRules
+  def initialize(test_passage)
+    @test_passage = test_passage
+    @user = test_passage.user
+    @test = test_passage.test
+  end
+
+  def check_for_reward
+    Badge.all.each do |badge|
+      case badge.rule
+      when 'category_complete'
+        reward(badge) if category_complete?(badge.parameter)
+      when 'first_try'
+        reward(badge) if first_try?
+      when 'level_complete'
+        reward(badge) if level_complete?(badge.parameter.to_i)
+      end
+    end
+  end
+
+  private
+
+  def reward(badge)
+    @user.badges << badge
+  end
+
+  def successful_test?
+    @test_passage.successfully?
+  end
+
+  # за прохождение теста определенной категории
+
+  def category_complete?(category)
+    successful_test? && @test.category.title == category
+  end
+
+  # за прохождение теста с первой попытки
+
+  def first_try?
+    successful_test? && @user.tests.where(test_id: @test.id).count == 1
+  end
+
+  # за прохождение теста определенного уровня
+
+  def level_complete?(level)
+    successful_test? && @test.level == level
+  end
+end
--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -14,7 +14,9 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
+      BadgeRules.new(@test_passage).check_for_reward
       TestsMailer.completed_test(@test_passage).deliver_now
+      
       redirect_to result_test_passage_path(@test_passage)
     else
       render :show
